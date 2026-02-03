a4cbf764a583feb3cccf2f766aaf0de0abbf6f91
BRYNN WILLARD <brynnwillard@BRYNNs-MacBook-Air-3.local>
2026-02-02 22:20:54 -0500

    adjusting all the hover effects

--- script-about.js
+++ script-about.js
@@ -34,10 +34,10 @@ navItems.forEach(item => {
 function changeNavColor(page) {
   const colors = {
     home: '#000000',
-    animation: '#000000',
-    production: '#000000',
-    resume: '#000000',
-    about: '#000000'
+    animation: '#5a7359',
+    production: '#de6f5f',
+    resume: '#836190',
+    about: '#6f6dac'
   };
   navItems.forEach(item => {
     if (item.dataset.page === page) {
--- script-home.js
+++ script-home.js
@@ -69,10 +69,10 @@ function changeLampImage(page) {
 function changeNavColor(page) {
   const colors = {
     home: '#000000',
-    animation: '#000000',
-    production: '#000000',
-    resume: '#000000',
-    about: '#000000'
+    animation: '#5a7359',
+    production: '#de6f5f',
+    resume: '#836190',
+    about: '#6f6dac'
   };
   navItems.forEach(item => {
     if (item.dataset.page === page) {
